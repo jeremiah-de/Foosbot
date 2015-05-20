30d99ff637f29b49ad0b2b0ddc95c0e7d70ff803
Jeremiah <jeremiah@doubleencore.com>
2015-05-20 12:34:34 -0600

    added /foos

--- index.js
+++ index.js
@@ -5,7 +5,45 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+
+	var request = require('request')
+
+    request({
+        url: 'https://api.hipchat.com/v2/room/1475277/notification',
+        method: 'POST',
+        qs: { 'auth_token': 'W22SSU8tc0lzHE3S854KKoNd4sTQAgpPai0Myd7V' },
+        json: { message: "@JeremiahGage", notify: true },
+        headers: { 'Content-Type' : 'application/json' }
+    }, function(error, response, body) {
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	    }
+	});
+
+	response.send('Hello World!');
+});
+
+app.get('/foos', function(request, response) {
+
+	var request = require('request')
+
+    request({
+        url: 'https://api.hipchat.com/v2/room/1475277/notification',
+        method: 'POST',
+        qs: { 'auth_token': 'W22SSU8tc0lzHE3S854KKoNd4sTQAgpPai0Myd7V' },
+        json: { message: "let's foos", notify: true },
+        headers: { 'Content-Type' : 'application/json' }
+    }, function(error, response, body) {
+	    if(error) {
+	        console.log(error);
+	    } else {
+	        console.log(response.statusCode, body);
+	    }
+	});
+
+	response.send('Foos!');
 });
 
 app.listen(app.get('port'), function() {
