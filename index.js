d66f1511b1069c4ae3fc075c0063a5cff18bf50b
Jeremiah <jeremiah@doubleencore.com>
2015-05-20 17:36:17 -0600

    push

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
 var app = express();
 
-var storage = require('node-persist');
-storage.initSync();
-
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+var mongoose = require ("mongoose");
+var uristring = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/HelloMongoose';
+var theport = process.env.PORT || 5000;
+mongoose.connect(uristring, function (err, res) {
+  if (err) {
+  	console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+  } else {
+	  console.log ('Succeeded connected to: ' + uristring);
+  }
+});
 
 app.get('/', function(request, response) {
 
