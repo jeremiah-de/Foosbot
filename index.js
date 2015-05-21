bb80dc478d742f1494572d543009df1cef4ce32c
Jeremiah <jeremiah@doubleencore.com>
2015-05-20 18:04:48 -0600

    push

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var multer = require('multer'); 
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(multer()); // for parsing multipart/form-data
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
@@ -39,8 +45,9 @@ app.get('/get', function (request, response)
 
 app.post('/foos', function(request, response)
 {
-	console.log(request.query);
-	response.send('Success');
+	console.log(request.body);
+  	res.json(request.body);
+  	response.send('Success');
 });
 
 app.listen(app.get('port'), function() {
